3b27d154a788273ea2bbffc13ad19130abc8f2b7
janlifelix <56059141+janlifelix@users.noreply.github.com>
2022-04-11 00:08:43 -0300

    Add files via upload

--- LivroUseAcabecaJava/src/capituloum/Main.java
+++ LivroUseAcabecaJava/src/capituloum/Main.java
@@ -4,6 +4,8 @@
  */
 package capituloum;
 
+import static java.lang.System.out;
+import java.util.Arrays;
 /**
  *
  * @author Janli Felix
@@ -14,7 +16,25 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        String [] bandaListaUm={"Deep Purple", "Black Sabbath", "Led Zeppelin", "Grand Funk", "Uriah Heep"};
+        String [] bandaListaDois={"Rush", "Hawkwind", "Ramones", "Sex Pistols", "Ac/Dc"};
+        String [] bandaListaTres={"Iron Maiden", "Nirvana", "Pearl Jam", "Alice In Chains", "AudioSlave"};
+        out.println("De manhã "+Arrays.toString(bandaListaUm));
+        out.println("De tarde "+Arrays.toString(bandaListaDois));
+        out.println("De noite "+Arrays.toString(bandaListaTres));
+        
+        int listaUm = bandaListaUm.length;
+        int listaDois = bandaListaDois.length;
+        int listaTres = bandaListaTres.length;
+        out.println("L1 = "+listaUm+"\nL2 = "+listaDois+"\nL3 = "+listaTres);
+        
+        int x =(int)(Math.random()*listaUm);
+        int y =(int)(Math.random()*listaDois);
+        int z =(int)(Math.random()*listaDois);
+        out.println("N1 = "+x+"\nN2 = "+y+"\nN3 = "+z);
+        
+        String playlist ="\nManhã "+bandaListaUm[x]+"\nTarde "+bandaListaDois[y]+"\nNoite "+bandaListaTres[z];
+        out.println("Minha playlist..."+playlist);
     }
     
 }
